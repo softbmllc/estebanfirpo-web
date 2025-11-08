d2ea5b4badab939a0c7855bddb1dc65ccedb6f86
Rodrigo Opalo Balog <r.opalo@icloud.com>
2025-11-08 16:15:26 -0500

    chore: remove old gracias page

--- src/app/gracias/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/gracias/page.tsx
-export default function GraciasPage() {
-    return (
-      <main className="min-h-screen flex flex-col justify-center items-center text-center px-6">
-        <h1 className="text-3xl font-bold mb-4">¡Gracias por contactarnos!</h1>
-        <p>Tu mensaje fue enviado correctamente. Nos pondremos en contacto contigo pronto.</p>
-      </main>
-    );
-  }
